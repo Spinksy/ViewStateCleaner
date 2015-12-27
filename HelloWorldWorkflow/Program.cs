5f9b6e30f86e9aed4b13cf13514efc185e676779
Glenn Spinks <glennspinks@gmail..com>
2015-12-27 14:02:49 +0800

    Initial commit

---
+++ HelloWorldWorkflow/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using System.Collections.Generic;
+
+namespace HelloWorldWorkflow
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+
+
+            Console.WriteLine("Are you male or female?");
+            string sex = Console.ReadLine();
+
+            if (String.Equals(sex[0].ToString().ToUpper(), "M"))
+            {
+                p.Sex = "Male";
+                p.Name = "Glenn";
+            }
+            else
+            {
+                p.Sex = "Female";
+                p.Name = "Gemma";
+            }
+
+            Activity workflow1 = new Workflow1();
+            WorkflowInvoker.Invoke(workflow1, new Dictionary<string, object> { { "Input", p } });
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Sex { get; set; }
+    }
+}
